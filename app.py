de45f2c72f32e0d5ce6ec1fd0a36c8569796fc11
Antonio V <avalverde@stroeer.de>
2024-06-26 11:43:46 +0200

    update app.py

--- app.py
+++ app.py
@@ -31,9 +31,11 @@ st.title("¡Unete a la familia de cafeteros!")
 
 c1,c2 = st.columns(2)
 with c1:
+    st.write("##")
     st.image("assets/logo.png")
 
 with c2:
+    st.write("##")
     nombre = st.text_input("Nombre*", placeholder="Tu nombre")
     email = st.text_input("Email*", placeholder="Tu mejor email")
     fecha_nacimiento = st.date_input("Fecha de nacimiento", help="Tu cumpleaños es importante para nosotros, te enviaremos un regalo especial ! 😉", value=None)
